99cc224ee672b21aa58c9891534949207665372d
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2022-04-17 20:03:18 -0300

    Docs 2

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import cors from 'cors';
 import express from 'express';
 import 'reflect-metadata';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 import { MongoDBConnect } from './database';
 import './shared/container';
 import { UpdateByRaiderIOController } from './modules/character/useCases/updateCharacterByRaiderIO/UpdateByRaiderIOController';
 import { router } from './routes';
-import swaggerFile from './swagger.json';
 
+const swaggerFile = YAML.load(path.resolve(__dirname, 'swagger.yaml'));
 const app = express();
 const updateByRaiderIO = new UpdateByRaiderIOController();
 
@@ -19,7 +21,13 @@ app.use(cors());
 async function connect() {
     MongoDBConnect();
     updateByRaiderIO.handle();
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    app.use(
+        '/api-docs',
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerFile, {
+            customCss: '.swagger-ui .topbar { display: none}',
+        }),
+    );
 
     app.use(router);
 
